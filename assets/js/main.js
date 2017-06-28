2c1c338d5c191653106888bdc19f8342d3f731a4
genarvaez <ge_naravez@hotmail.com>
2017-06-28 14:07:14 -0400

    maqueta inicial termianda

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+	$("#boton").click(function(e){
+		e.preventDefault();
+		var tarea = $("#tarea").val();
+
+		if(tarea == ""){
+			alert("El campo no puede estár vacío");
+		}
+		else{
+			$(".list").append("<li><input type='checkbox'>" + "<span>" + tarea+"</span>" + "<button class='remove'>Remove</button></li>");
+			$("#tarea").val("");
+
+
+
+		}
+
+		$(".remove").click(function(){
+		$(this).siblings().remove();
+		this.remove();
+		})
+
+	})
+
+
+})
