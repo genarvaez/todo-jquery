a10ec2627e44a26daadff5a2031a84a4b28e435a
genarvaez <ge_naravez@hotmail.com>
2017-06-29 01:11:10 -0400

    funcion de cambio de tarea completa e incompleta casi lista

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,29 +3,30 @@ $(document).ready(function(){
 		e.preventDefault();
 		var tarea = $("#tarea").val();
 		var completed = $(".completed");
+		var date = new Date();
 
 		if(tarea == ""){
 			alert("El campo no puede estár vacío");
 		}
 		else{
-			$(".list").append("<li><input type='checkbox' class='check'>" + "<span id='tareas'>" + tarea+"</span>" + "<button class='remove'>Remove</button></li>");
+			$(".list").append("<li><input type='checkbox' id='myCheckbox' class=filled-in' /><label  for='myCheckbox'></label>" +
+			"<span id='tareas'>" + tarea+"</span><span class='onHover'>"+date.getDate() + '/' + (date.getMonth() +1) + '/' + date.getFullYear() +"</span>"+ 
+			"<button class='remove'><i class='fa fa-trash-o' aria-hidden='true'></i></button></li>");
 			$("#tarea").val("");
-
-			$(".remove").click(function(){
-		$(this).siblings().remove();
-		this.remove();
-		})
-
-		$(".tareas").click(function(){
-			$("completed").append(this);
-		})
-		
 		}
 
+		$(".remove").click(function(){
+		$(this).parent().remove();
+		})
 
 
-
+		$('[type="checkbox"]').change(function(){
+	    	if(this.checked){
+	    		completed.append($(this).parent());
+	    	}
+	    	else{
+	    		$(".list").append($(this).parent());
+	    	}
+ 		});
 	})
-
-
 }) 	
